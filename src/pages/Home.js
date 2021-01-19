0ff5dd9c5b10988fc52ee6d1730f9aadf02e3e93
khanhnoi <nqkhanh1998@gmail.com>
2021-01-19 17:45:52 +0700

    pj init

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import name from "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+      <div className="home">
+        <h1>This IS hOME PAGE</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home;
